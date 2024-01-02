87b56959a2046b989fb1aa4d1dd77fcc7e556f92
ximen502 <ximen502@126.com>
2024-01-02 20:35:01 +0800

    [feature]炮弹类击中基地老鹰增加音效；增加击中钢铁音效文件到package

---
+++ src/game/AC.kt
@@ -0,0 +1,16 @@
+package game
+
+import java.applet.AudioClip
+
+/**
+ * @Class AC
+ * @Description
+ * @Author xsc
+ * @Date 2024/01/02 下午03:01
+ * @Version 1.0
+ */
+object AC {
+    // 击中钢铁或多命坦克的音效
+    var hitAC: AudioClip? = null
+    var enemyDieAC: AudioClip? = null
+}
--- src/game/Shells.kt
+++ src/game/Shells.kt
@@ -879,6 +879,7 @@ class Shells : GameObject() {
             mapArray[28][19] = 0
             mapArray[29][18] = 0
             mapArray[29][19] = 0
+            AC.enemyDieAC?.play()
         }
         return hit
     }
