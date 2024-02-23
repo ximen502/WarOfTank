9fc80e205d90e6adbd0b8a0b1086e4e2597f688b
ximen502 <ximen502@126.com>
2024-02-23 19:31:25 +0800

    资源使用逻辑完善

--- src/game/Map.java
+++ src/game/Map.java
@@ -84,6 +84,8 @@ public class Map {
                 }
                 r++;//行号加1
             }
+            br.close();
+            is.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
